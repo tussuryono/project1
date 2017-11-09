b6bceaded55c6068af4fc3d5e38b8ff4abf74f6d
tus suryono <tus.suryono@gmail.com>
2017-11-09 18:56:07 +0700

    update macam - macam

---
+++ test1.js
@@ -0,0 +1,12 @@
+var nim = "960123456789";
+
+switch(nim){
+    case "960123456789" :
+        console.log("Tus Suryono");
+        break;
+    case "960123456780" :
+        console.log("Brahmastyo Ray Suryono");
+        break;
+    default :
+        console.log("Bukan siapa - siap ......");
+}
